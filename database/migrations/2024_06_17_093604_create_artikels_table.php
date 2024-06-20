c1a2488043f2e72a52ff8f4110fa307df83a9aeb
reonaldi152 <118794017+reonaldi152@users.noreply.github.com>
2024-06-20 07:41:10 +0700

    fix:error load image cms

--- database/migrations/2024_06_17_093604_create_artikels_table.php
+++ database/migrations/2024_06_17_093604_create_artikels_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('artikels', function (Blueprint $table) {
             $table->id();
             $table->string('title')->nullable();
-            $table->string('description')->nullable();
+            $table->longText('description')->nullable();
             $table->string('link')->nullable();
             $table->string('image')->nullable();
             $table->timestamps();
