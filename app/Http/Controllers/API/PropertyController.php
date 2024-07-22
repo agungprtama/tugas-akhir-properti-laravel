d03941a8c15e76eb0623b45fa34a702b475112be
reonaldi152 <reonaldi@team.superfk.co>
2024-07-22 07:21:03 +0700

    property by id

--- app/Http/Controllers/API/PropertyController.php
+++ app/Http/Controllers/API/PropertyController.php
@@ -101,7 +101,7 @@ class PropertyController extends Controller
     public function show($id)
     {
         try {
-            $property = Property::findOrFail($id)->with('user')->get();
+            $property = Property::with('user')->findOrFail($id);
             // return response()->json($property);
             return ResponseFormatter::success($property, 'Berhasil mendapatkan data property');
         } catch (ModelNotFoundException $e) {
